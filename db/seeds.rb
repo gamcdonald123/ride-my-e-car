dc1177eae7053d1012eacbd1458462ca32bab4d8
Guy McDonald <guy@goodfishgroup.com>
2024-02-28 15:19:02 +0000

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,6 +42,8 @@ puts "Creating users..."
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: "password")
 end
 
+User.create(first_name: "Guy", last_name: "McDonald", email: "gamcdonald@icloud.com", password: "password")
+
 puts "Creating cars..."
 
 20.times do
