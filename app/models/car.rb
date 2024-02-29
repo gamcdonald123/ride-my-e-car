ba832b3d067a007bd098e242b08f317b94aba48d
Mirela Mustic <mirela.mustic00@gmail.com>
2024-02-29 17:33:26 +0000

    display reviews

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :reviews, through: :bookings
+  has_many :reviews
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 end
